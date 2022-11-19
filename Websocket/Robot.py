c38cf20338d0a3e984decc458331f5922c69c86a
GZod01 <95311062+GZod01@users.noreply.github.com>
2022-11-19 10:17:36 +0100

    ok

--- Websocket/Robot.py
+++ Websocket/Robot.py
@@ -4,19 +4,31 @@ class driver:
         dri.set_esc(pin,vitesse)
 class Robot:
     Driver=driver()
+    pingauche=0
+    pindroite=0
     current_vitessed=0
     current_vitesseg=0
     vitessegauche=0
     vitessedroite=0
+    def init(self,pgauche,pdroite):
+        self.pingauche=pgauche
+        self.pindroite=pdroite
+    def checkforpin(self):
+        if (self.pingauche==0 or self.pindroite==0) and (self.pingauche==0 and self.pindroite==0):
+            print('there is an error, the pin are not defined')
+        else:
+            pass
     def moteurG(self,n):
+        self.checkforpin()
         n=int(n)
         driver=self.Driver
-        driver.setvitesse(3,n)
+        driver.setvitesse(self.pingauche,n)
         self.current_vitesseg=n
     def moteurD(self,n):
+        self.checkforpin()
         n=int(n)
         driver=self.Driver
-        driver.setvitesse(4,n)
+        driver.setvitesse(self.pindroite,n)
         self.current_vitessed=n
     def Avancer(self,a):
         a=int(a)
