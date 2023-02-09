615ae24c7c470a98d42ebb34e5b4bfb6a2296753
GZod01 <95311062+GZod01@users.noreply.github.com>
2023-02-09 15:39:23 +0100

    ok

---
+++ gzod01.fr/tranformpage.js
@@ -0,0 +1,24 @@
+function transformpage(docstr, titleofdoc){
+    let toreturn = `
+    <!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="//api.gzod01.fr/pictures/gzod01.png">
+    <link rel="stylesheet" href="//api.gzod01.fr/css/style.css">
+    <script src="//api.gzod01.fr/scripts/main.js"></script>
+    <title>${titleofdoc}</title>
+</head>
+<body>
+    <div id="header"></div>
+    <div class=content>
+    ${docstr}
+    </div>
+    <div id="footer"></div>
+</body>
+</html>
+    `
+}
+window.onload = () => {document.querySelector(':root').innerHTML = transformpage(document.getElementById('content'))}
