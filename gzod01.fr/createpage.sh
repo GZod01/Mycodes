ac19f96ff266862f5085ea1b49bcb43f599ad1ca
GZod01 <95311062+GZod01@users.noreply.github.com>
2023-02-09 16:52:39 +0100

    ok

--- gzod01.fr/createpage.sh
+++ gzod01.fr/createpage.sh
@@ -2,7 +2,23 @@
 originaltitle=$1
 title= ${originaltitle//"-"/" "}
 cat > $1 <<-END
-    This is line one.
-    This is line two.
-    This is line three.
+    <!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="//api.gzod01.fr/pictures/gzod01.png">
+    <link rel="stylesheet" href="//api.gzod01.fr/css/style.css">
+    <script src="//api.gzod01.fr/scripts/main.js"></script>
+    <title>${title}</title>
+</head>
+<body>
+    <div id="header"></div>
+    <div class=content>
+        YOUR HTML HERE
+    </div>
+    <div id="footer"></div>
+</body>
+</html>
 END
