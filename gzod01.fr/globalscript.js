d1ce0e31c37adc22f2e2ada3ccba1ce152798e12
GZod01 <95311062+GZod01@users.noreply.github.com>
2023-02-09 15:29:44 +0100

    ok

--- gzod01.fr/globalscript.js
+++ gzod01.fr/globalscript.js
@@ -1,28 +1,59 @@
+var getfooter = `<footer>
+<div class=footerleft>
+    <a href="/">Accueil</a><br>
+    <a href="/games.html">Jeux</a><br>
+    <a href="/services.html">Services</a><br>
+    <a href="/projects.html">Projets</a><br>
+</div>
+<div class=footerright>
+    <a href="/contact.html">Me contacter</a><br>
+    <a href="/sitemap.html">Plan Du Site</a><br>
+    <a href="/about.html">A Propos</a><br>
+    <a href="/legal.html">Mentions Légales</a><br>
+</div>              
+</footer>`
+var getheader = `<header>
+<div class="nav">
+    <a href="/">Accueil</a>
+    <a href="/games.html">Jeux</a>
+    <a href="/services.html">Services</a>
+    <a href="/projects.html">Projets</a>
+    <a href="/contact.html">Me contacter</a>
+    <a href="/sitemap.html">Plan Du Site</a>
+    <a href="/about.html">A Propos</a>
+    <a href="/legal.html">Mentions Légales</a>
+</div>
+<span class=themebutton onclick=switchtheme()>Switch the theme</span>
+</header>`
 function getpartials(){
+    // let default_header = document.createElement('header')
+    // let getheader = fetch('//api.gzod01.fr/parts/header.html').then(function (response) {
+    //     // The API call was successful!
+    //     return response.text();
+    // }).then(function (html) {
+    //     let parser = new DOMParser();
+    //     let doc = parser.parseFromString(html, 'text/html');
+    //     return doc
+    // }).catch(function (err) {
+    //     console.warn('Something went wrong.', err);
+    // });
+    // default_header.appendChild(getheader)
+    // let default_footer = document.createElement('footer')
+    // let getfooter = fetch('//api.gzod01.fr/parts/footer.html').then(function (response) {
+    //     // The API call was successful!
+    //     return response.text();
+    // }).then(function (html) {
+    //     let parser = new DOMParser();
+    //     let doc = parser.parseFromString(html, 'text/html');
+    //     return doc
+    // }).catch(function (err) {
+    //     console.warn('Something went wrong.', err);
+    // });
+    // default_footer.appendChild(getfooter)
     let default_header = document.createElement('header')
-    let getheader = fetch('//api.gzod01.fr/parts/header.html').then(function (response) {
-        // The API call was successful!
-        return response.text();
-    }).then(function (html) {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(html, 'text/html');
-        return doc
-    }).catch(function (err) {
-        console.warn('Something went wrong.', err);
-    });
-    default_header.appendChild(getheader)
     let default_footer = document.createElement('footer')
-    let getfooter = fetch('//api.gzod01.fr/parts/footer.html').then(function (response) {
-        // The API call was successful!
-        return response.text();
-    }).then(function (html) {
-        let parser = new DOMParser();
-        let doc = parser.parseFromString(html, 'text/html');
-        return doc
-    }).catch(function (err) {
-        console.warn('Something went wrong.', err);
-    });
-    default_footer.appendChild(getfooter)
+    default_header.innerHTML = getheader
+    default_footer.innerHTML = getfooter
     document.getElementById('header').replaceWith(default_header)
     document.getElementById('footer').replaceWith(default_footer)
 }
